46d8a2be044707a68cc35fbf69462fc4c42c510f
Dmytro Tsuran <dtsuran@gmail.com>
2018-07-24 00:17:00 +0300

    Use Bulma styles.

--- src/main/resources/static/core/js/hello.js
+++ src/main/resources/static/core/js/hello.js
@@ -1,21 +1,29 @@
-function post(path, params, method) {
-	method = method || "post"; 
+$(document).ready(function() {
+    $(".navbar-burger").click(function() {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
 
-	var form = document.createElement("form");
-	form.setAttribute("method", method);
-	form.setAttribute("action", path);
+    });
 
-	for ( var key in params) {
-		if (params.hasOwnProperty(key)) {
-			var hiddenField = document.createElement("input");
-			hiddenField.setAttribute("type", "hidden");
-			hiddenField.setAttribute("name", key);
-			hiddenField.setAttribute("value", params[key]);
+    function post(path, params, method) {
+        method = method || "post";
 
-			form.appendChild(hiddenField);
-		}
-	}
+        var form = document.createElement("form");
+        form.setAttribute("method", method);
+        form.setAttribute("action", path);
 
-	document.body.appendChild(form);
-	form.submit();
-}
+        for ( var key in params) {
+            if (params.hasOwnProperty(key)) {
+                var hiddenField = document.createElement("input");
+                hiddenField.setAttribute("type", "hidden");
+                hiddenField.setAttribute("name", key);
+                hiddenField.setAttribute("value", params[key]);
+
+                form.appendChild(hiddenField);
+            }
+        }
+
+        document.body.appendChild(form);
+        form.submit();
+    }
+});
